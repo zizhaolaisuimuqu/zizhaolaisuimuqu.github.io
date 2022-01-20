3f9021f36d421cd266b9e38977098f4fade0cc62
ZogeMung <1533567830@qq.com>
2022-01-20 15:30:18 +0800

    Update

--- source/js/load-aplayer.js
+++ source/js/load-aplayer.js
@@ -1,17 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let apContainer = document.createElement("div");
     apContainer.id = "aplayer";
     document.body.append(apContainer);
     const ap = new APlayer({
-      container: document.getElementById("aplayer"),
-      fixed: true,
-      audio: [
-        {
-          name: "春物",
-          artist: "雪之下雪乃",
-          url: "https://raw.githubusercontent.com/zizhaolaisuimuqu/zizhaolaisuimuqu.github.io/hexo/zizhaolaisuimuqu.github.io/themes/yun/source/%E3%82%84%E3%81%AA%E3%81%8E%E3%81%AA%E3%81%8E%20-%20%E6%98%A5%E6%93%AC%E3%81%8D.mp3",
-          cover: "https://raw.githubusercontent.com/zizhaolaisuimuqu/zizhaolaisuimuqu.github.io/hexo/zizhaolaisuimuqu.github.io/themes/yun/source/Yukino.png",
-        },
-      ],
+        container: document.getElementById("aplayer"),
+        fixed: true,
+        audio: [{
+            name: "春物",
+            artist: "雪之下雪乃",
+            url: "https://raw.githubusercontent.com/zizhaolaisuimuqu/zizhaolaisuimuqu.github.io/hexo/themes/yun/source/%E3%82%84%E3%81%AA%E3%81%8E%E3%81%AA%E3%81%8E%20-%20%E6%98%A5%E6%93%AC%E3%81%8D.mp3",
+            cover: "https://raw.githubusercontent.com/zizhaolaisuimuqu/zizhaolaisuimuqu.github.io/hexo/themes/yun/source/Yukino.png",
+        }, ],
     });
-  });
+});
